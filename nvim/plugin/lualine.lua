65fa866943aeb9a8e32ecf90ec47680ece9f56c9
Leander Gangsø <leander.gangso98@gmail.com>
2026-06-15 14:49:09 +0200

    chore: manual sync latest nvim repo changes

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,22 +1,21 @@
 vim.pack.add({
-	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/nvim-lualine/lualine.nvim",
 })
 
 require("lualine").setup({
 	options = {
 		theme = "auto",
+		globalstatus = true,
+		disabled_filetypes = { "alpha", "oil" },
 	},
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff" },
 		lualine_c = {
-			{
-				"filename",
-				path = 1,
-			},
+			{ "filename", path = 1 },
+			"diagnostics",
 		},
-		lualine_x = { "encoding", "fileformat" },
+		lualine_x = { "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
