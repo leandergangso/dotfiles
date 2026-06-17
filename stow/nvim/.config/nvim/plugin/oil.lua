2058ad29edf730c66564368df80198701a749658
Leander Gangsø <leander.gangso98@gmail.com>
2026-06-17 10:32:05 +0200

    nvim: oil hidden files

--- stow/nvim/.config/nvim/plugin/oil.lua
+++ stow/nvim/.config/nvim/plugin/oil.lua
@@ -2,6 +2,12 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/oil.nvim" },
 })
 
+local hidden_files = {
+	["."] = true,
+	[".."] = true,
+	[".git"] = true,
+}
+
 require("oil").setup({
 	float = {
 		padding = 2,
@@ -11,7 +17,10 @@ require("oil").setup({
 		border = "rounded",
 	},
 	view_options = {
-		show_hidden = false,
+		show_hidden = true,
+		is_hidden_file = function(name)
+			return hidden_files[name] == true
+		end,
 	},
 })
 
