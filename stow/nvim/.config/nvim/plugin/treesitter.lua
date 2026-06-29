829c24dcbd1bbefc91df7d19843c44b80ec30968
Leander Gangsø <leander.gangso98@gmail.com>
2026-06-29 13:55:25 +0200

    nvim: lazy loading

--- stow/nvim/.config/nvim/plugin/treesitter.lua
+++ stow/nvim/.config/nvim/plugin/treesitter.lua
@@ -1,6 +1,14 @@
 vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
+	{ src = "https://github.com/romus204/tree-sitter-manager.nvim" },
+})
+
+require("tree-sitter-manager").setup({
+	auto_install = true,
+	sync_install = false,
+	highlight = true,
+	use_repo_queries = true,
 })
 
 require("treesitter-context").setup({
@@ -17,26 +25,4 @@ require("treesitter-context").setup({
 	on_attach = nil,
 })
 
-vim.pack.add({
-	{ src = "https://github.com/romus204/tree-sitter-manager.nvim" },
-}, {
-	load = function(plugin)
-		local opts = { desc = "[T]reesitter Manager", silent = true }
-
-		vim.keymap.set("n", "<leader>T", function()
-			vim.cmd.packadd(plugin.spec.name)
-
-			require("tree-sitter-manager").setup({
-				auto_install = true,
-				sync_install = false,
-				highlight = true,
-				use_repo_queries = true,
-			})
-
-			vim.cmd("TSManager")
-
-			-- rebind so future hits just the command
-			vim.keymap.set("n", "<leader>T", "<cmd>TSManager<CR>", opts)
-		end, opts)
-	end,
-})
+vim.keymap.set("n", "<leader>T", "<cmd>TSManager<CR>", { desc = "[T]reesitter Manager", silent = true })
