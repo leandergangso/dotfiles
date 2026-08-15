d371ed574a1603b8db5cb7154c52cd2bf7526fa1
Leander Gangsø <leander.gangso98@gmail.com>
2026-08-15 12:23:33 +0200

    nvim: add csvview plugin

---
+++ stow/nvim/.config/nvim/plugin/csvview.lua
@@ -0,0 +1,24 @@
+vim.pack.add({
+	{ src = "https://github.com/hat0uma/csvview.nvim" },
+}, {
+	load = function(plugin)
+		vim.api.nvim_create_autocmd("FileType", {
+			once = true,
+			pattern = "csv",
+			callback = function()
+				vim.cmd.packadd(plugin.spec.name)
+				require("csvview").setup({
+					parser = { comments = { "#", "//" } },
+					keymaps = {
+						textobject_field_inner = { "if", mode = { "o", "x" } },
+						textobject_field_outer = { "af", mode = { "o", "x" } },
+						jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+						jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+						jump_next_row = { "<Enter>", mode = { "n", "v" } },
+						jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+					},
+				})
+			end,
+		})
+	end,
+})
