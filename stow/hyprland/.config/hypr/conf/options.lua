41f65d0f66b96d05f1498c23cf9119e0a1f7f1eb
Leander Gangsø <leander.gangso98@gmail.com>
2026-09-02 16:22:38 +0200

    hyprland: chore

--- stow/hyprland/.config/hypr/conf/options.lua
+++ stow/hyprland/.config/hypr/conf/options.lua
@@ -32,8 +32,11 @@ hl.config({
 	},
 	input = {
 		kb_layout = "us,no",
-		follow_mouse = 0,
+		follow_mouse = 2,
 		sensitivity = 0,
+	},
+	cursor = {
+		no_warps = false,
 	},
 	scrolling = {
 		fullscreen_on_one_column = true,
@@ -43,20 +46,39 @@ hl.config({
 	},
 })
 
+local monitorLeft = "HDMI-A-1"
+local monitorCenter = "DP-2 "
+local monitorRight = "DP-1"
+
+hl.config({
+	workspace = {
+		{ id = 1, monitor = monitorLeft },
+		{ id = 2, monitor = monitorLeft },
+		{ id = 3, monitor = monitorLeft },
+		{ id = 4, monitor = monitorCenter },
+		{ id = 5, monitor = monitorCenter },
+		{ id = 6, monitor = monitorCenter },
+		{ id = 7, monitor = monitorCenter },
+		{ id = 8, monitor = monitorRight },
+		{ id = 9, monitor = monitorRight },
+		{ id = 10, monitor = monitorRight },
+	},
+})
+
 hl.animation({ leaf = "global", enabled = true, speed = 5, bezier = "default" })
-hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
+--hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
+--hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
+--hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
 hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
-hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
-hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
+--hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
+--hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
+--hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
+--hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
+--hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
+--hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
+--hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
+--hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
+--hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+--hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
+--hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+--hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
