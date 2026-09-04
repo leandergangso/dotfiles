9a7270f5aa2781afd70a955d0291d1a63d7675b0
Leander Gangsø <leander.gangso98@gmail.com>
2026-09-04 11:48:19 +0200

    nvim: docker and podman filetypes

--- stow/nvim/.config/nvim/lua/filetypes.lua
+++ stow/nvim/.config/nvim/lua/filetypes.lua
@@ -1,16 +1,17 @@
 vim.filetype.add({
-    pattern = {
-        -- Dockerfile variations
-        ["[Dd]ockerfile.*"] = "dockerfile",
+	pattern = {
+		-- Dockerfile variations
+		["[Dd]ockerfile.*"] = "dockerfile",
+		["[Cc]ontainerfile.*"] = "dockerfile",
 
-        -- Docker Compose, Podman Compose, and OCI Standard Compose files
-        [".*docker%-compose.*%.yaml"] = "yaml",
-        [".*docker%-compose.*%.yml"] = "yaml",
-        [".*podman%-compose.*%.yaml"] = "yaml",
-        [".*podman%-compose.*%.yml"] = "yaml",
-        [".*compose.*%.yaml"] = "yaml",
-        [".*compose.*%.yml"] = "yaml",
-    },
+		-- Docker Compose, Podman Compose, and OCI Standard Compose files
+		[".*docker%-compose.*%.yaml"] = "yaml",
+		[".*docker%-compose.*%.yml"] = "yaml",
+		[".*podman%-compose.*%.yaml"] = "yaml",
+		[".*podman%-compose.*%.yml"] = "yaml",
+		[".*compose.*%.yaml"] = "yaml",
+		[".*compose.*%.yml"] = "yaml",
+	},
 })
 
 vim.filetype.add({
